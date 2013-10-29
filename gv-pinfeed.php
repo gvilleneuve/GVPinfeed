4e569988aacf4de06866263898c6d72097d78034
Mike Almond <almond@fastnorth.com>
2013-10-29 12:25:22 -0400

    WordPress code style update

--- gv-pinfeed.php
+++ gv-pinfeed.php
@@ -11,101 +11,101 @@
  * Domain Path: /languages
  */
 
-if (!defined('GV_PINFEED_PLUGIN_VERSION')) {
-    define('GV_PINFEED_PLUGIN_VERSION', '1.0.0');
+if ( ! defined( 'GV_PINFEED_PLUGIN_VERSION' ) ) {
+	define( 'GV_PINFEED_PLUGIN_VERSION', '1.0.0' );
 }
-if (!defined('GV_PINFEED_PLUGIN_URL')) {
-    define('GV_PINFEED_PLUGIN_URL', plugin_dir_url(__FILE__));
+if ( ! defined( 'GV_PINFEED_PLUGIN_URL' ) ) {
+	define( 'GV_PINFEED_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
-if (!defined('GV_PINFEED_PLUGIN_PATH')) {
-    define('GV_PINFEED_PLUGIN_PATH', plugin_dir_path(__FILE__));
+if ( ! defined( 'GV_PINFEED_PLUGIN_PATH' ) ) {
+	define( 'GV_PINFEED_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 }
 
 function gv_pinfeed_plugin_setup() {
 
-    /**
-     * Include Plugin Options.
-     */
-    require_once(GV_PINFEED_PLUGIN_PATH . 'options.php');
+	/**
+	 * Include Plugin Options.
+	 */
+	require_once( GV_PINFEED_PLUGIN_PATH . 'options.php' );
 
-    /**
-     * Include Menu Page.
-     */
-    //require_once( GV_PINFEED_PLUGIN_PATH . 'inc/menu.php' );
+	/**
+	 * Include Menu Page.
+	 */
+	//require_once( GV_PINFEED_PLUGIN_PATH . 'inc/menu.php' );
 
-    /**
-     * Include Custom Widget.
-     */
-    require_once(GV_PINFEED_PLUGIN_PATH . 'widget.php');
-    /**
-     * Include Request for the Twitter Feed.
-     */
-    //require_once( GV_PINFEED_PLUGIN_PATH . 'inc/get_tweets.php' );
+	/**
+	 * Include Custom Widget.
+	 */
+	require_once( GV_PINFEED_PLUGIN_PATH . 'widget.php' );
+	/**
+	 * Include Request for the Twitter Feed.
+	 */
+	//require_once( GV_PINFEED_PLUGIN_PATH . 'inc/get_tweets.php' );
 
-    /**
-     * Include Shortcode.
-     */
-    //require_once( GV_PINFEED_PLUGIN_PATH . 'inc/shortcode.php' );
+	/**
+	 * Include Shortcode.
+	 */
+	//require_once( GV_PINFEED_PLUGIN_PATH . 'inc/shortcode.php' );
 
-    /**
-     * Load Text Domain for Translations.
-     */
-    //load_plugin_textdomain( 'gv_pinfeed', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
+	/**
+	 * Load Text Domain for Translations.
+	 */
+	//load_plugin_textdomain( 'gv_pinfeed', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 
 }
 
-add_action('plugins_loaded', 'gv_pinfeed_plugin_setup', 15);
+add_action( 'plugins_loaded', 'gv_pinfeed_plugin_setup', 15 );
 
-if (!function_exists('gv_pinfeed_plugin_scripts')):
+if ( ! function_exists( 'gv_pinfeed_plugin_scripts' ) ):
 
-    /**
-     * Enqueue plugin scripts and styles.
-     */
-    function gv_pinfeed_scripts() {
+	/**
+	 * Enqueue plugin scripts and styles.
+	 */
+	function gv_pinfeed_scripts() {
 
-        // Queues the main CSS file.
-        wp_register_style(
-            'gv-pinfeed-plugin',
-        GV_PINFEED_PLUGIN_URL . 'pin.css',
-            array(),
-            GV_PINFEED_PLUGIN_VERSION,
-            'all'
-        );
-        wp_register_style(
-            'gv-pinfeed-plugin-default-style',
-        GV_PINFEED_PLUGIN_URL . 'pin-default.css',
-            array(),
-            GV_PINFEED_PLUGIN_VERSION,
-            'all'
-        );
+		// Queues the main CSS file.
+		wp_register_style(
+			'gv-pinfeed-plugin',
+				GV_PINFEED_PLUGIN_URL . 'pin.css',
+			array(),
+			GV_PINFEED_PLUGIN_VERSION,
+			'all'
+		);
+		wp_register_style(
+			'gv-pinfeed-plugin-default-style',
+				GV_PINFEED_PLUGIN_URL . 'pin-default.css',
+			array(),
+			GV_PINFEED_PLUGIN_VERSION,
+			'all'
+		);
 
-        // Queues the js to make the slider happen
-        wp_register_script('jquery-latest', '//code.jquery.com/jquery-latest.min.js', array(), null, false);
-        wp_register_script('unslider', '//unslider.com/unslider.min.js', array(), null, false);
-        wp_register_script('pin-widget-js', '/wp-content/plugins/GVPinfeed/pin.js', array(), null, false);
+		// Queues the js to make the slider happen
+		wp_register_script( 'jquery-latest', '//code.jquery.com/jquery-latest.min.js', array(), null, false );
+		wp_register_script( 'unslider', '//unslider.com/unslider.min.js', array(), null, false );
+		wp_register_script( 'pin-widget-js', '/wp-content/plugins/GVPinfeed/pin.js', array(), null, false );
 
-        // Enqueue Stylesheet for Admin Pages
-        if (is_admin()) {
-            wp_enqueue_style('gv-pinfeed-plugin');
-        }
-    }
+		// Enqueue Stylesheet for Admin Pages
+		if ( is_admin() ) {
+			wp_enqueue_style( 'gv-pinfeed-plugin' );
+		}
+	}
 
-    add_action('wp_enqueue_scripts', 'gv_pinfeed_scripts');
-    add_action('admin_enqueue_scripts', 'gv_pinfeed_scripts');
+	add_action( 'wp_enqueue_scripts', 'gv_pinfeed_scripts' );
+	add_action( 'admin_enqueue_scripts', 'gv_pinfeed_scripts' );
 
 endif;
 
 /**
  * Add a link to the plugin screen, to allow users to jump straight to the settings page.
  */
-function gv_pinfeed_plugin_meta($links) {
+function gv_pinfeed_plugin_meta( $links ) {
 
-    $links[] = '<a href="' . admin_url('options-general.php?page=gv-pinfeed') . '">' . __(
-        'Settings',
-        'gv_pinfeed'
-    ) . '</a>';
+	$links[] = '<a href="' . admin_url( 'options-general.php?page=gv-pinfeed' ) . '">' . __(
+				'Settings',
+				'gv_pinfeed'
+			) . '</a>';
 
-    return $links;
+	return $links;
 }
 
-add_filter('plugin_action_links_gv-pinfeed/gv-pinfeed.php', 'gv_pinfeed_plugin_meta');
+add_filter( 'plugin_action_links_gv-pinfeed/gv-pinfeed.php', 'gv_pinfeed_plugin_meta' );
--- options.php
+++ options.php
@@ -5,47 +5,47 @@
 
 function gv_pinfeed_options_init() {
 
-    register_setting(
-        'gv_pinfeed_options', // Options group
-        'gv_pinfeed_options' // Database option
-    );
+	register_setting(
+		'gv_pinfeed_options', // Options group
+		'gv_pinfeed_options' // Database option
+	);
 }
 
-add_action('admin_init', 'gv_pinfeed_options_init');
+add_action( 'admin_init', 'gv_pinfeed_options_init' );
 
 /**
  * Change the capability required to save the 'gv_pinfeed_options' options group.
  */
-function gv_pinfeed_option_capability($capability) {
+function gv_pinfeed_option_capability( $capability ) {
 
-    return 'manage_options';
+	return 'manage_options';
 }
 
-add_filter('option_page_capability_gv_pinfeed_options', 'gv_pinfeed_option_capability');
+add_filter( 'option_page_capability_gv_pinfeed_options', 'gv_pinfeed_option_capability' );
 
 /**
  * Returns the options array for the 'gv_pinfeed_options' option group.
  */
 function gv_pinfeed_get_options() {
 
-    $saved = (array)get_option('gv_pinfeed_options');
+	$saved = (array) get_option( 'gv_pinfeed_options' );
 
-    $defaults = apply_filters('gv_pinfeed_get_options', $defaults);
+	$defaults = apply_filters( 'gv_pinfeed_get_options', $defaults );
 
-    $options = wp_parse_args($saved, $defaults);
-    $options = array_intersect_key($options, $defaults);
+	$options = wp_parse_args( $saved, $defaults );
+	$options = array_intersect_key( $options, $defaults );
 
-    return $options;
+	return $options;
 }
 
 /**
  * Sanitize and validate form input. Accepts an array, return a sanitized array.
  */
-function gv_pinfeed_options_validate($input) {
+function gv_pinfeed_options_validate( $input ) {
 
-    $options = gv_pinfeed_get_options();
+	$options = gv_pinfeed_get_options();
 
-    $output = array();
+	$output = array();
 
-    return apply_filters('gv_pinfeed_options_validate', $output, $options);
+	return apply_filters( 'gv_pinfeed_options_validate', $output, $options );
 }
